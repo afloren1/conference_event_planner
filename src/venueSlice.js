5b4712f3141179e229099753a6c23ad9f1285afb
afloren1 <bluemoon66063@gmail.com>
2024-07-17 16:31:27 -0400

    changed the images from venueSlice.js

--- src/venueSlice.js
+++ src/venueSlice.js
@@ -5,31 +5,31 @@ export const venueSlice = createSlice({
   name: "venue",
   initialState: [
     {
-      img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
+      img: "https://i.pinimg.com/564x/0f/6a/95/0f6a958fb3c032823a9f9cc675da9071.jpg",
       name: "Conference Room (Capacity:15)",
       cost: 3500,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/event-venue-1597531_640.jpg",
+      img: "https://i.pinimg.com/564x/f0/4e/35/f04e35bdbbab3a1b7301b90a7363665e.jpg",
       name: "Auditorium Hall (Capacity:200)",
       cost: 5500,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/convention-center-3908238_640.jpg",
+      img: "https://i.pinimg.com/564x/69/70/4b/69704b0983791f6108154578cefef4c0.jpg",
       name: "Presentation Room (Capacity:50)",
       cost: 700,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/chairs-2181916_640.jpg",
+      img: "https://i.pinimg.com/564x/7a/62/61/7a6261674b12306d166914fc3f3a423d.jpg",
       name: "Large Meeting Room (Capacity:10)",
       cost: 900,
       quantity: 0,
     },
     {
-      img: "https://pixabay.com/images/download/laptops-593296_640.jpg",
+      img: "https://i.pinimg.com/564x/80/6c/66/806c663f7e64d2f2ccac2071026f8043.jpg",
       name: "Small Meeting Room (Capacity:5)",
       cost: 1100,
       quantity: 0,
